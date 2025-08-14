e38dea3ee53104eef3b46d5f0c686442ddbeaf33
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-08-14 18:03:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/semana01/MainActivity.kt
@@ -0,0 +1,140 @@
+package com.example.semana01
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.semana01.ui.theme.Semana01Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Semana01Theme {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color =  Color(0xFFF5F5F5)
+                ){
+                    LoginScreen()
+                }
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreen(){
+    var userName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column (
+        modifier = Modifier.
+            fillMaxSize()
+            .padding(horizontal = 32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+    ){
+        Image(
+            painter = painterResource(id= R.drawable.ic_launcher_foreground),
+            contentDescription = "Logo",
+            modifier = Modifier.size(50.dp),
+            contentScale = ContentScale.Fit
+
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text (
+            text = "Duoc UC",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text (
+            text = "Iniciar sesión para continuar",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            color = Color.DarkGray
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        OutlinedTextField(
+            value = userName,
+            onValueChange = { userName = it },
+            label = { Text("Nombre de usuario") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth().height(50.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFB4E330)),
+            shape = RoundedCornerShape(4.dp)
+        ){
+            Text("Iniciar sesión", fontSize = 16.sp, color = Color.White)
+
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Recuperar contraseña",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color(0xFF1E88E5),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.clickable{
+
+            }
+        )
+
+
+    }
+
+}
