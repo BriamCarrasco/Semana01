d56aa9423b4cfefeae675d11ed511e6b4a38a3cc
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-09-01 20:20:24 -0400

    Se genera Bottombar.

---
+++ app/src/main/java/com/example/semana01/screens/topbar/BottomBar.kt
@@ -0,0 +1,60 @@
+package com.example.semana01.screens.topbar
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.semana01.nav.NavRouter
+
+@Preview
+@Composable
+fun BottomBar(navController: NavController) {
+    BottomAppBar(
+        modifier = Modifier.height(66.dp),
+        containerColor = Color(0xFFFFFFFF),
+        tonalElevation = 16.dp
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            IconButton(onClick = {navController.navigate(NavRouter.HomeScreen.route)}) {
+                Icon(
+                    imageVector = Icons.Filled.Home,
+                    contentDescription = "Home",
+                    tint = Color.Black,
+                )
+            }
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "Notificaciones",
+                    tint = Color.Black
+                )
+            }
+
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Filled.Settings,
+                    contentDescription = "Settings",
+                    tint = Color.Black
+                )
+            }
+        }
+    }
+}
