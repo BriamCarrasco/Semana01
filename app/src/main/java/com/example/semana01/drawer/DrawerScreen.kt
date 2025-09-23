b27a110233d7f5881d1180ca1ebdefa93276b4d0
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-09-22 21:07:58 -0300

    Se crea menú lateral

---
+++ app/src/main/java/com/example/semana01/drawer/DrawerScreen.kt
@@ -0,0 +1,94 @@
+package com.example.semana01.drawer
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import kotlinx.coroutines.launch
+
+@Composable
+fun DrawerScreen(
+    navRouter: NavController,
+    content: @Composable (openDrawer: () -> Unit) -> Unit
+) {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(260.dp)
+                    .background(Color(0xFFF5F5F5))
+                    .padding(top = 48.dp, start = 16.dp, end = 16.dp, bottom = 16.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Minuta nutricional",
+                    color = Color(0xFFFD9C00),
+                    modifier = Modifier.padding(bottom = 24.dp)
+                )
+                Text(
+                    text = "Favoritos",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            scope.launch { drawerState.close() }
+                        }
+                        .padding(vertical = 12.dp)
+                )
+
+                Text(
+                    text = "Confgurar perfil",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            scope.launch { drawerState.close() }
+                        }
+                        .padding(vertical = 12.dp)
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Button(
+                    onClick = {
+                        navRouter.popBackStack()
+                        navRouter.navigate("loginScreen")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFFFC107),
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text("Cerrar sesión")
+                }
+            }
+        }
+    ) {
+        content { scope.launch { drawerState.open() } }
+    }
+}
